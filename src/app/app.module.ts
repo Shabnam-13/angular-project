1bcdfc72f707d01c873870c15d803211b5e9c383
Shabnam-13 <shabnamah@code.edu.az>
2022-03-29 12:32:29 +0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {
-  FontAwesomeModule,
-  FaIconLibrary,
-} from '@fortawesome/angular-fontawesome';
+// import {
+//   FontAwesomeModule,
+//   FaIconLibrary,
+// } from '@fortawesome/angular-fontawesome';
 import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, FontAwesomeModule],
+  imports: [BrowserModule, FormsModule],
+  // imports: [BrowserModule, FormsModule, FontAwesomeModule],
   declarations: [AppComponent, HelloComponent, BookListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(library: FaIconLibrary) {
-    // Add an icon to the library for convenient access in other components
-  }
+  // constructor(library: FaIconLibrary) {
+  //   // Add an icon to the library for convenient access in other components
+  // }
 }
